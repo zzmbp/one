ff1eec71cbf03679cb2612e3c04af928a66b7bd4
zzmbp <zzmacbookpro@hotmail.com>
2019-10-13 11:27:47 +0800

    第一个项目，刚刚建的。。。。

--- src/com/example/Hello.java
+++ src/com/example/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
-        System.out.println("/n");
+        System.out.println("。。。。");
     }
 }
