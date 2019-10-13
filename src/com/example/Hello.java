ffb0198b1dde636636d15c0d94a27343521bc24a
zzmbp <zzmacbookpro@hotmail.com>
2019-10-13 11:54:42 +0800

    第一个项目，第二次提交。。。。

--- src/com/example/Hello.java
+++ src/com/example/Hello.java
@@ -2,7 +2,7 @@ package com.example;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("world");
+        System.out.println("java");
         System.out.println("。。。。");
     }
 }
